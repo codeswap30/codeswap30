4e9eeb10e51ac46299be8b296211cbe34eed366c
unknown <codedwapmaster@gmail.com>
2024-01-01 12:12:57 +0100

    rentwise admin

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RentWise.Admins.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
